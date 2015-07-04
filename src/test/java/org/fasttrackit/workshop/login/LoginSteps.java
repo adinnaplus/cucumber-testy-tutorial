a28278e6b146d9fb0deeb71ad56a8325ff3499db
Adina <adina.moldovan@altom.ro>
2015-07-04 13:41:04 +0300

    Using testy and switching the use from LoginPage to LoginView

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -5,7 +5,9 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.fasttrackit.util.TestBase;
 import org.fasttrackit.util.TestBaseNative;
+import org.fasttrackit.workshop.LoginView;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -16,18 +18,22 @@ import org.slf4j.LoggerFactory;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class LoginSteps extends TestBaseNative {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(LoginSteps.class);
+public class LoginSteps extends TestBase {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginSteps.class);
 
-    LoginPage loginPage;
+//    WEBDRIVER PAGE OBJECTS
+//    LoginPage loginPage;
+//
+//    public LoginSteps() {
+//        initPage();
+//    }
+//
+//    public void initPage() {
+//        loginPage = PageFactory.initElements(driver, LoginPage.class);
+//    }
 
-    public LoginSteps() {
-        initPage();
-    }
-
-    public void initPage() {
-        loginPage = PageFactory.initElements(driver, LoginPage.class);
-    }
+//    TESTY OBJECT
+    private LoginView loginView = new LoginView();
 
     @Given("^I navigate to the login page$")
     public void I_navigate_to_the_login_page()  {
@@ -36,33 +42,32 @@ public class LoginSteps extends TestBaseNative {
 
     @And("^fill in valid credentials$")
     public void fill_in_valid_credentials()  {
-        loginPage.enterCredentials("eu@fast.com", "eu.pass");
+        loginView.enterCredentials("eu@fast.com", "eu.pass");
     }
 
     @When("^clicking the login button$")
     public void clicking_the_login_button()  {
-        loginPage.clickOnLoginButton();
+        loginView.clickOnLoginButton();
     }
 
     @Then("^the user is logged in$")
     public void the_user_is_logged_in()  {
         // Cucumber doens't tell the differece between a fail on an assert and a fail on the step
         WebElement logoutText = driver.findElement(By.xpath("//*[contains(text(), 'Logout')]"));
-        System.out.println(logoutText.getAttribute("class"));
     }
 
     @Given("^fill in invalid credentials$")
     public void fill_in_invalid_credentials()  {
-        loginPage.enterCredentials("eu@fast.com", "eroare");
+        loginView.enterCredentials("eu@fast.com", "eroare");
     }
 
     @When("^I enter email \"([^\"]*)\" and password \"([^\"]*)\"$")
     public void I_enter_email_and_password(String email, String password)  {
-        loginPage.enterCredentials(email, password);
+        loginView.enterCredentials(email, password);
     }
 
     @Then("^the login error message \"([^\"]*)\" is displayed$")
     public void the_login_error_message_is_displayed(String errorMessage)  {
-        assertThat(loginPage.getErrorMessage(), is(errorMessage)); // Matei ar pune si assets in LoginPage
+        assertThat(loginView.getErrorMessage(), is(errorMessage)); // Matei ar pune si assets in LoginPage
     }
 }
